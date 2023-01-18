069335b69062279f8dcee7ab80c801a24085e7c0
Heri Setyawan <herisetyawan233@gmail.com>
2023-01-18 17:00:33 +0700

    feet: collect assets which needed

---
+++ lib/src/constant/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const Color white = Color(0xFFFFFFFF);
+const Color primary = Color(0xFF2B637B);
+const Color inputColor = Color.fromRGBO(104, 103, 119, 0.36);
+const Color black = Color(0xFF04021D);
+const Color blue = Color(0xFF554AF0);
